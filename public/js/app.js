2f219acb8dec002b55a66760d074d04e45ed75b3
punipuni21 <punihobby@gmail.com>
2021-03-14 01:41:15 +0900

    プレイヤースコアの背景を変更（暫定版）

--- public/js/app.js
+++ public/js/app.js
@@ -7033,7 +7033,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, ".player-name {\n  background-color: chartreuse;\n}", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, ".player-name {\n  background-color: chartreuse;\n}\n\n.player-score {\n  background-color: gray;\n}", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
