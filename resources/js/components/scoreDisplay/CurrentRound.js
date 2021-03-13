ca5b2b448d928ec1ad7592f4ed7fe60adb05ff1f
punipuni21 <punihobby@gmail.com>
2021-03-14 03:36:02 +0900

    現在の局を示す部分の作成

---
+++ resources/js/components/scoreDisplay/CurrentRound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './CurrentRound.css'
+
+class CurrentRound extends React.Component{
+
+  render(){
+
+
+    return(
+      <div>hoge</div>
+    );
+  }
+}
+
+export default CurrentRound;
